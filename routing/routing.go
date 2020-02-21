d344eab17c53460662e36ea043df9a63ce3b4112
David Tillery <dtillery@paloaltonetworks.com>
2020-02-21 17:50:57 -0500

    Missed removing the sort import.

--- routing/routing.go
+++ routing/routing.go
@@ -35,111 +35,110 @@ package routing
 
 import "fmt"
 import "net"
-import "sort"
 import "strings"
 
 type Route struct {
-	Default bool
-	SrcNet  *net.IPNet
-	DstNet  *net.IPNet
-	Gateway net.IP
-	Iface   *net.Interface
+    Default bool
+    SrcNet  *net.IPNet
+    DstNet  *net.IPNet
+    Gateway net.IP
+    Iface   *net.Interface
 }
 
 type route_slice []*Route
 
 func (r route_slice) Len() int {
-	return len(r)
+    return len(r)
 }
 
 func (r route_slice) Swap(i, j int) {
-	r[i], r[j] = r[j], r[i]
+    r[i], r[j] = r[j], r[i]
 }
 
 func (r route_slice) Less(i, j int) bool {
-	a := r[i]
-	b := r[j]
+    a := r[i]
+    b := r[j]
 
-	if a.Default {
-		return true
-	}
+    if a.Default {
+        return true
+    }
 
-	if b.Default {
-		return false
-	}
+    if b.Default {
+        return false
+    }
 
-	a_len, _ := a.DstNet.Mask.Size()
-	b_len, _ := b.DstNet.Mask.Size()
-	if a_len < b_len {
-		return false
-	}
+    a_len, _ := a.DstNet.Mask.Size()
+    b_len, _ := b.DstNet.Mask.Size()
+    if a_len < b_len {
+        return false
+    }
 
-	return true
+    return true
 }
 
 // Return the default IPv4 address of a network interface.
 func (r *Route) GetIfaceIPv4Addr() (net.IP, error) {
-	iface := r.Iface
+    iface := r.Iface
 
-	addrs, err := iface.Addrs();
-	if err != nil {
-		return nil, err
-	}
+    addrs, err := iface.Addrs();
+    if err != nil {
+        return nil, err
+    }
 
-	for _, a := range addrs {
-		if ipnet, ok := a.(*net.IPNet); ok {
-			if ip4 := ipnet.IP.To4(); ip4 != nil {
-				return ip4, nil
-			}
-		}
-	}
+    for _, a := range addrs {
+        if ipnet, ok := a.(*net.IPNet); ok {
+            if ip4 := ipnet.IP.To4(); ip4 != nil {
+                return ip4, nil
+            }
+        }
+    }
 
-	return nil, fmt.Errorf("No address found")
+    return nil, fmt.Errorf("No address found")
 }
 
 // Return the default IPv6 address of a network interface.
 func (r *Route) GetIfaceIPv6Addr() (net.IP, error) {
-	iface := r.Iface
+    iface := r.Iface
 
-	addrs, err := iface.Addrs();
-	if err != nil {
-		return nil, err
-	}
+    addrs, err := iface.Addrs();
+    if err != nil {
+        return nil, err
+    }
 
-	for _, a := range addrs {
-		if ipnet, ok := a.(*net.IPNet); ok {
-			if ip6 := ipnet.IP.To16(); ip6 != nil {
-				return ip6, nil
-			}
-		}
-	}
+    for _, a := range addrs {
+        if ipnet, ok := a.(*net.IPNet); ok {
+            if ip6 := ipnet.IP.To16(); ip6 != nil {
+                return ip6, nil
+            }
+        }
+    }
 
-	return nil, fmt.Errorf("No address found")
+    return nil, fmt.Errorf("No address found")
 }
 
 func (r *Route) String() string {
-	var parts []string
+    var parts []string
 
-	if r.Default {
-		parts = append(parts, "default")
-	} else if r.DstNet != nil {
-		parts = append(parts, r.DstNet.String())
-	}
+    if r.Default {
+        parts = append(parts, "default")
+    } else if r.DstNet != nil {
+        parts = append(parts, r.DstNet.String())
+    }
 
-	if r.SrcNet != nil {
-		src := fmt.Sprintf("from %s", r.DstNet.String())
-		parts = append(parts, src)
-	}
+    if r.SrcNet != nil {
+        src := fmt.Sprintf("from %s", r.DstNet.String())
+        parts = append(parts, src)
+    }
 
-	if r.Gateway != nil {
-		gateway := fmt.Sprintf("via %s", r.Gateway.String())
-		parts = append(parts, gateway)
-	}
+    if r.Gateway != nil {
+        gateway := fmt.Sprintf("via %s", r.Gateway.String())
+        parts = append(parts, gateway)
+    }
 
-	if r.Iface != nil {
-		iface := fmt.Sprintf("dev %s", r.Iface.Name)
-		parts = append(parts, iface)
-	}
+    if r.Iface != nil {
+        iface := fmt.Sprintf("dev %s", r.Iface.Name)
+        parts = append(parts, iface)
+    }
 
-	return strings.Join(parts, " ")
+    return strings.Join(parts, " ")
 }
